52939d41174fa7aa3ecefd918cd98694eb43052c
Amin Saedi <amin.di.sam@gmail.com>
2024-03-29 18:38:13 -0400

    Added ansible code

--- install.sh
+++ install.sh
@@ -4,17 +4,40 @@ set -e
 
 TEMP_DIR=$(mktemp -d)
 
-PWD=$(pwd)
+ROOT_DIR=$(pwd)
+
+
+# ask for confirmation
 
 # cd to ./terraform dir and run terraform init
 
-cd $PWD/terraform
+cd $ROOT_DIR/terraform
 
 terraform init > /dev/null 2>&1
 
+start_time=$(date +%s)
+
 terraform apply -auto-approve > /dev/null 2>&1
+
+end_time=$(date +%s)
 
 terraform output -json > $TEMP_DIR/terraform-output.json
 
-# cd to ./ansible dir and run ansible-playbook
+if [ $((end_time - start_time)) -gt 10 ]; then
+  echo "Sleeping for 60 seconds to allow the Droplet to start"
+  sleep 60
+fi
 
+cd $ROOT_DIR/ansible
+
+GITLAB_HOST=$(jq -r '."gitlab-server-fqdn".value' $TEMP_DIR/terraform-output.json)
+
+ansible-playbook -e "temp_dir=$TEMP_DIR gitlab_host=$GITLAB_HOST" playbooks/all.yml
+
+
+echo "GitLab is ready at: https://$GITLAB_HOST"
+
+PASSWORD=$(cat $TEMP_DIR/initial_root_password | grep -oP 'Password: \K.*')
+
+echo "Username: root"
+echo "Password: $PASSWORD"
